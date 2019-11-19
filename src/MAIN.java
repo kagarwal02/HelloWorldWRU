42f95c3ebf96a70ed9e3f0b38a9939931a06d88b
wurdahl <wurdahl@gmail.com>
2019-11-19 14:26:49 -0600

    Deletion and LMAO

--- src/MAIN.java
+++ src/MAIN.java
@@ -2,11 +2,7 @@ public class MAIN {
 
     public static void main(String Args[]){
 
-        System.out.println("Hello World!");
-
-        for(int i=1; i<11;i++){
-            System.out.println(i);
-        }
+        //LMAO
 
     }
 
